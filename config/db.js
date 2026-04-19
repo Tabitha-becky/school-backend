4f39c700767c5915ffad970be1696552174ef462
Tabitha-becky <159132941+Tabitha-becky@users.noreply.github.com>
2026-04-19 20:44:11 +0300

    fix db.js to use Railway DB_URL

--- config/db.js
+++ config/db.js
@@ -21,7 +21,7 @@ pool.connect((err, client, release) => {
   if (err) {
     console.error('Database connection failed:', err.message);
   } else {
-    console.log('Database connected successfully');
+    console.log('✅ Database connected successfully');
     release();
   }
 });
